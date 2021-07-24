6a2cde76637acca2e3a3bb307e6977cb67a39055
Reilly Thompson <thompson.reilly@yahoo.com>
2021-07-23 17:44:28 -0700

    first commit

--- src/app.js
+++ src/app.js
@@ -1,17 +1,28 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
 
-// TODO: Follow instructions in the checkpoint to implement ths API.
+const flips = require("./data/flips-data")
+const counts = require("./data/counts-data")
+
+const countsRouter = require("./counts/counts.router")
+const flipsRouter = require("./flips/flips.router")
+
+app.use("/counts", countsRouter)
+app.use("/flips", flipsRouter)
+
+
+app.use(express.json())
 
 // Not found handler
 app.use((request, response, next) => {
-  next(`Not found: ${request.originalUrl}`);
-});
+  next({ status: 404, message: `Not found: ${request.originalUrl}` })
+})
 
 // Error handler
 app.use((error, request, response, next) => {
-  console.error(error);
-  response.send(error);
-});
+  console.error(error)
+  const { status = 500, message = "Something went wrong!" } = error
+  response.status(status).json({ error: message })
+})
 
-module.exports = app;
+module.exports = app
